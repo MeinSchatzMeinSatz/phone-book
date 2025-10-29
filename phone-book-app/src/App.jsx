9c4df9dd5fd7c655868bf176bf6d547a2319d6ee
Liebe_Chaeso <d131215@naver.com>
2025-10-29 10:26:08 +0900

    테일윈드 세팅

--- phone-book-app/src/App.jsx
+++ phone-book-app/src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  /*
+  1. 왼쪽에는 연락처 등록하는 폼이, 오른쪽에는 연락처 리스트와 search 창이 있다
+  2. 리스트에 유저 이름과 전화번호를 추가할 수 있다
+  3. 리스트에 아이템이 몇 개 있는지 보인다
+  4. 사용자가 유저를 이름 검색으로 찾을 수 있다
+  */
+  return (
+    <>
+      <h1 className="text-3xl">연락처</h1>
+    </>
+  );
 }
 
 export default App;
